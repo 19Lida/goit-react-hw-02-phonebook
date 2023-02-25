e01435ba45d4afccfa501fad2fc74cbf594542fe
19Lida <lida19melnyk@gmail.com>
2023-02-25 18:42:25 +0200

    edits the contacts feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,6 +32,9 @@ export class App extends Component {
       alert(`${name} is already in contacts`);
       return;
     }
+    this.setState(prevState => ({
+      contacts: [data, ...prevState.contacts],
+    }));
   };
   onChangeFilter = e => {
     this.setState({ filter: e.target.value });
